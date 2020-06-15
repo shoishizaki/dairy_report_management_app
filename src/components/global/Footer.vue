b883a2d22201838b26b1bc5f20dca939cd165371
shoishizaki <shoishizaki1997@gmail.com>
2020-06-16 02:54:25 +0900

    Create footer tab

---
+++ src/components/global/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <md-button to="">Home</md-button>
+    <md-button to="" class="md-primary">Create</md-button>
+    <md-button to="" class="md-accent">Settings</md-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonRouter'
+}
+</script>
