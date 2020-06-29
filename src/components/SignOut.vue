dac75bc9c9a643c6312f209243fa8a6c550bbc44
shoishizaki <shoishizaki1997@gmail.com>
2020-06-29 21:14:26 +0900

    create sign out function

---
+++ src/components/SignOut.vue
@@ -0,0 +1,26 @@
+<template>
+  <p>Please use again...</p>
+</template>
+
+<script>
+import firebase from '../firebase/firestore'
+
+export default {
+  created: function() {
+    const self = this
+    firebase
+      .auth()
+      .signOut()
+      .then(function() {
+        // 正常にsignupできた時の処理
+        self.$router.push('/login')
+      })
+  }
+}
+</script>
+
+<style scoped>
+p {
+  margin-left: 60px;
+}
+</style>
--- src/components/global/Header.vue
+++ src/components/global/Header.vue
@@ -1,10 +1,13 @@
 <template>
-  <md-tabs md-sync-route class="md-primary">
-    <md-tab id="tab-home" md-label="Home" to="/" exact></md-tab>
-    <md-tab id="tab-post" md-label="Create Report" to="/create"></md-tab>
-    <md-tab id="tab-list" md-label="Report List" to="/list"></md-tab>
-    <md-tab id="tab-settings" md-label="settings"></md-tab>
-    <md-tab id="tab-signup" md-label="Sign Up" to="/signup"></md-tab>
-    <md-tab id="tab-login" md-label="Login" to="/login"></md-tab>
-  </md-tabs>
+  <div>
+    <md-tabs md-sync-route class="md-primary">
+      <md-tab id="tab-home" md-label="Home" to="/" exact></md-tab>
+      <md-tab id="tab-post" md-label="Create Report" to="/create"></md-tab>
+      <md-tab id="tab-list" md-label="Report List" to="/list"></md-tab>
+      <md-tab id="tab-settings" md-label="settings"></md-tab>
+      <md-tab id="tab-signup" md-label="Sign Up" to="/signup"></md-tab>
+      <md-tab id="tab-login" md-label="Login" to="/login"></md-tab>
+      <md-tab id="tab-signout" md-label="Signout" to="/signout"></md-tab>
+    </md-tabs>
+  </div>
 </template>
